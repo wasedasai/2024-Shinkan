de9dc07188737c7e844d9aafee93a4f4cc32ea5c
korokoroyupin <korokorobuta021@gmail.com>
2024-03-16 23:33:32 +0900

    finish/schedule

--- static/js/schedule.js
+++ static/js/schedule.js
@@ -1,17 +1,5 @@
-// if(window.matchMedia("(max-width: 1023px)").matches){
-//     $(function () {
-//         /*タイトルをクリックすると*/
-//         $(".js-sukejuru").on("click", function() {
-//       　　 /*クリックした隣の要素を開閉する*/
-//           $(this).next(".sukejuru-info").slideToggle(200);
-//       　　/*タイトルにopenクラスの追加、削除を行って矢印の向きを変える*/
-//           $(this).toggleClass("open",200);
-//         });
-//       });
-// }
-
-// if(window.matchMedia("(max-width:1023px)").matches){
-$('.blocktitle-green').on('click', function() {
+if(window.matchMedia("(max-width:1023px)").matches){
+$('.js-sukejuru').on('click', function() {
   var findElm = $(this).next(".sukejuru-info");
   $(findElm).slideToggle();
 
@@ -20,5 +8,4 @@ $('.blocktitle-green').on('click', function() {
   }else{
     $(this).addClass('close');
   }
-});
-
+});}
